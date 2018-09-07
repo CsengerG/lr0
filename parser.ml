75d904a5d903652e7eebcdcbd02c18c26ab4857e
Geza Csenger <csengergeza@gmail.com>
2018-09-07 09:08:56 +0200

    add closure and goto

--- parser.ml
+++ parser.ml
@@ -1,57 +1,25 @@
 exception InvalidInputException;
 
-datatype Terminal = Number |
-                    Plus |
-                    Minus |
-                    Star |
-                    ExclMark |
-                    Cos |
-                    Exponent |
-                    Point |
-                    OpPar |
-                    ClPar |
-                    Eps
+datatype Terminal = Id | Plus | Star | OpPar | ClPar | Eps
+datatype NonTerminal = Start | E | Term | F
 
-datatype NonTerminal = Start | expr | E | E1 | E2 | E3 | E4 | E5
-datatype GrammarSymbol = T  of Terminal
-                       | NT of NonTerminal
-                       | EndMarker
-
+datatype GrammarSymbol = T  of Terminal | NT of NonTerminal | EndMarker | ItemDot
 datatype ProductionRule = PR of NonTerminal * GrammarSymbol list
-
-(*
-val prodrules = [
-    PR(E, [T Plus, NT E]),
-    PR(E, [T Minus, NT E]),
-    PR(E, [NT E, T Plus, NT E]),
-    PR(E, [NT E, T Minus, NT E]),
-    PR(E, [NT E, T Star, NT E]),
-    PR(E, [T Number])]
-*)
 
 val prodrules = [
     PR(Start, [NT E]),
 
-    PR(E, [T Plus, NT E]),
-    PR(E, [T Minus, NT E]),
-    PR(E, [NT E1, T Plus, NT E]),
-    PR(E, [NT E1]),
+    PR(E, [NT E, T Plus, NT Term]),
+    PR(E, [NT Term]),
 
-    PR(E1, [NT E2, T Minus, NT E1]),
-    PR(E1, [NT E2]),
+    PR(Term, [NT Term, T Star, NT F]),
+    PR(Term, [NT F]),
 
-    PR(E2, [NT E2, T Star, NT E3]),
-    PR(E2, [NT E3]),
-
-    PR(E3, [T Cos, NT E4]),
-    PR(E3, [NT E4]),
-
-    PR(E4, [NT E5, T ExclMark]),
-    PR(E4, [NT E5]),
-
-    PR(E5, [T Number]),
-    PR(E5, [T OpPar, NT E, T ClPar])
+    PR(F, [T OpPar, NT E, T ClPar]),
+    PR(F, [T Id])
 ]
+
+datatype Item = I of NonTerminal * GrammarSymbol list
 
 datatype 'a Set = S of 'a list
 
@@ -66,9 +34,13 @@ fun insert(S [], y) = S [y]
       if x=y then S (x::xs) 
       else let val (S L) = insert(S xs, y) in S (x::L) end
 
+fun isEmpty(S []) = true
+  | isEmpty _ = false
+
 fun union(S [], set) = set 
   | union(set, S []) = set
   | union(S s1, S s2) = foldl (fn (x,acc) => insert(acc,x)) (S s1) s2
+
 
 fun unionWithout(leaveOut, S s1, S s2) = foldl (fn (x,acc) => 
                                        if x = leaveOut then acc
@@ -77,6 +49,7 @@ fun unionWithout(leaveOut, S s1, S s2) = foldl (fn (x,acc) =>
 fun filter _ [] = []
   | filter p (x::xs) = if p x then x :: filter p xs 
                        else filter p xs
+fun diff(S l1, S l2) = S (filter (fn x => not (inSet(S l2, x))) l1)
 
 fun all _ [] = true
   | all p (x::xs) = p x andalso all p xs
@@ -139,3 +112,52 @@ fun follow EndMarker = raise NoFollow
     in
       foldl (fn (x,acc) => union(x,acc)) (S []) sets
     end
+
+exception MaybeException
+
+fun setmap f (S l) = S (map f l)
+fun setfilter f (S l) = S (filter f l)
+
+fun closure set =
+  let
+    fun closureUtil was (S []) = was
+      | closureUtil was (S ((curr as (I(l,r)))::rest)) =
+      let
+        fun symbolafterdot [] = []
+          | symbolafterdot [x] = []
+          | symbolafterdot (ItemDot::x::_) = [x]
+          | symbolafterdot (x::xs) = symbolafterdot xs
+
+        fun productionsWith [] = []
+          | productionsWith [x] = filter (fn (PR(l,_)) => NT l=x) prodrules 
+          | productionsWith _ = raise MaybeException
+
+        val symbolAfterDot = symbolafterdot r
+        val prods = productionsWith symbolAfterDot
+        val nexts = map (fn (PR(head, right)) => I(head, ItemDot :: right)) prods
+        val (S newnexts) = diff(S nexts, was)
+      in
+        closureUtil (insert(was, curr)) (S (rest @ newnexts))
+      end
+  in closureUtil (S []) set end
+
+val cl = closure (S [I(Start, [ItemDot, NT E])])
+fun len [] = 0 | len (x::xs) = 1 + len xs
+fun size (S l) = len l
+exception NoDot
+fun goto i x = 
+  let
+    fun dotbeforex [] = false
+      | dotbeforex (ItemDot::s::ss) = s = x
+      | dotbeforex (s::ss) = dotbeforex ss
+
+    fun moveDotToRight [] = raise NoDot
+      | moveDotToRight (ItemDot::s::ss) = s::ItemDot::ss
+      | moveDotToRight (s::ss) = s :: moveDotToRight ss
+
+    val withDotBeforeX = setfilter (fn I(_, r) => dotbeforex r) i
+  in
+    closure (setmap (fn (I(l,r)) => I(l, moveDotToRight r)) withDotBeforeX)
+  end
+
+val g = goto (S [I(Start, [NT E, ItemDot]), I(E, [NT E, ItemDot, T Plus, NT Term])]) (T Plus)
